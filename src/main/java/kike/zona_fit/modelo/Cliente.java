5972eb3424919f5596916fc0f6dba9543f5c73b5
Enrique García <enrique.garcia@redpontis.com>
2025-03-11 21:43:17 -0600

    refactor: 💡 se configura la entidad con las propiedades

--- src/main/java/kike/zona_fit/modelo/Cliente.java
+++ src/main/java/kike/zona_fit/modelo/Cliente.java
@@ -1,5 +1,24 @@
 package kike.zona_fit.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
+@Entity
+@Data
+@NoArgsConstructor // agrega un constructor vacio
+@AllArgsConstructor // agrega un constructor que reciba todos las propiedades o argumentos
+@ToString
+@EqualsAndHashCode
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String apellido;
+    private Integer membresia;
 }
